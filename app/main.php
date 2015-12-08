c11e4b373e1929e79d7013b02d6c9588150de194
sjohnkennedy <suhirthan@hotmail.com>
2015-12-08 12:48:19 +0530

    Initial setup and commit

---
+++ app/main.php
@@ -0,0 +1,9 @@
+<?php
+
+include_once '../libs/ussd/MoUssdReceiver.php';
+include_once '../libs/ussd/MtUssdSender.php';
+
+include_once '../libs/log.php';
+
+ini_set('error_log', 'ussd-app-error.log');
+
